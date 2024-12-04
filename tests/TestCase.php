75a4b80e7e052b43448416fbe8383e7787bf231d
Undecane <undecane@outlook.com>
2024-12-04 12:28:45 +0000

    wip

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,8 +2,8 @@
 
 namespace Zing\HttpClient\Tests;
 
-if (class_exists('PHPUnit_Framework_TestCase') && ! class_exists(BaseTestCase::class)) {
-    class_alias('PHPUnit_Framework_TestCase', BaseTestCase::class);
+if (class_exists('PHPUnit_Framework_TestCase') && ! class_exists('PHPUnit\Framework\TestCase')) {
+    class_alias('PHPUnit_Framework_TestCase', 'PHPUnit\Framework\TestCase');
 }
 
 use PHPUnit\Framework\TestCase as BaseTestCase;
